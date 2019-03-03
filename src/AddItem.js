eb998f4df673d5b8118e9c68c56cc23a1b6ce9d2
einomies26 <einokamarainen@hotmail.com>
2019-03-03 11:44:02 +0200

    initial commit

---
+++ src/AddItem.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import SkyLight from 'react-skylight';
+
+// This component will use the React Skylight modal form. Inside the React Skylight
+// component in the render() method, we add two input fields (product and amount)
+// and a button that calls the addItem function. To be able to call the addItem
+// function that is in the App.js component, we have to pass it in a prop when
+// rendering the AddItem component. Outside the React Skylight component, we will
+// add a button that opens the modal form when it is pressed. This button is the
+// only visible element when the component is rendered initially and it calls the
+// React Skylight show() method to open the modal form. We also have to handle the
+// change event of the input fields, so that we can access the values that have
+// been typed. When the button inside the modal form is clicked, the addItem
+// function is called and the modal form is closed using the React Skylight hide()
+// method. The function creates an object from the input field values and calls
+// the App.js component's addItem function, which finally add a new item to the
+// state array and re-renders the user interface.
+
+class AddItem extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    // Create new shopping item and calls addItem function.
+    // Finally close the modal form
+    addItem = () => {
+        const item = {
+            product: this.state.product,
+            amount: this.state.amount
+        };
+        this.props.additem(item);
+        this.addform.hide();
+    }
+
+    handleChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+    render() {
+        return (
+            <div>
+                <section>
+                    <button onClick={() => this.addform.show()}>Add Item</button>
+                </section>
+                <SkyLight hideOnOverlayClicked ref={ref => this.addform = ref} title="Add item">
+                    <input type="text" name="product" onChange={this.handleChange} placeholder="product" /><br />
+                    <input type="text" name="amount" onChange={this.handleChange} placeholder="amount" /><br />
+                    <button onClick={this.addItem}>Add</button>
+                </SkyLight>
+            </div>
+        );
+    }
+
+}
+
+export default AddItem;
--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import AddItem from './AddItem';
 
 class App extends Component {
+
+  constructor(props) {
+    super();
+    this.state = { items: [] };
+  }
+
+  addItem = (item) => {
+    this.setState({ items: [item, ...this.state.items] });
+  }
+
+  // AddItem component added to the render() method and pass the addItem function
+  // in a prop to the AddItem component. At the beginning of the method, we transform
+  // items to listItems (<li></li>) using the map function.
   render() {
+    const listItems = this.state.items.map((item, index) =>
+      <li key={index}>{item.product} {item.amount}</li>)
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h2>Shopping list</h2>
+        <AddItem additem={this.addItem} />
+        <ul>{listItems}</ul>
       </div>
     );
   }
+
 }
 
 export default App;
